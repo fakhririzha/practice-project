13587e699c7c74941f2da9e79cbe9c5e2ab45847
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-10-14 16:41:30 +0700

    Lecture 95 - Adding a User List Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import AddUser from "./Users/AddUser";
+import UsersList from "./Users/UsersList";
 
 const App = () => {
+	const users = [];
 	return (
 		<div>
 			<AddUser></AddUser>
+			<UsersList users={[]}></UsersList>
 		</div>
 	);
 };
