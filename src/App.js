d0ea28bdd2d343337b257d23695152657d6226ea
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-10-16 14:11:26 +0700

    Restructuring files in source folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import AddUser from "./Users/AddUser";
-import UsersList from "./Users/UsersList";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 const App = () => {
 	const [usersList, setUsersList] = useState([]);
