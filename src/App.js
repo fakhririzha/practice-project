8a04f43d8348348d498a9d970ec0e28c72169f42
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-10-16 14:29:11 +0700

    Lecture 102 - React fragments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
@@ -15,10 +15,10 @@ const App = () => {
 	};
 
 	return (
-		<div>
+		<React.Fragment>
 			<AddUser onAddUser={addUserHandler}></AddUser>
 			<UsersList users={usersList}></UsersList>
-		</div>
+		</React.Fragment>
 	);
 };
 
