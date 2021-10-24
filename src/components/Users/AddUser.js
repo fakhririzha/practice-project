27eceb3528d60acf170eb0d2646f3d234932b955
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2021-10-24 16:08:39 +0700

    Lecture 105 - Working with "refs"

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Wrapper from "../Helpers/Wrapper";
 
 import Button from "../UI/Button";
@@ -8,38 +8,49 @@ import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-	const [enteredName, setEnteredUsername] = useState("");
-	const [enteredAge, setEnteredAge] = useState("");
+	// const [enteredName, setEnteredUsername] = useState("");
+	// const [enteredAge, setEnteredAge] = useState("");
+
+	const nameInputRef = useRef("");
+	const ageInputRef = useRef("");
+
 	const [formError, setFormError] = useState();
 
 	const addUserHandler = (event) => {
 		event.preventDefault();
-		if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
+		const enteredUserName = nameInputRef.current.value;
+		const enteredUserAge = ageInputRef.current.value;
+		if (
+			enteredUserName.trim().length === 0 ||
+			enteredUserAge.trim().length === 0
+		) {
 			setFormError({
 				title: "Invalid input",
 				message: "Please enter a valid name and age (non-empty values).",
 			});
 			return;
 		}
-		if (+enteredAge < 1) {
+		if (+enteredUserAge < 1) {
 			setFormError({
 				title: "Invalid input",
 				message: "Please enter a valid age (> 0).",
 			});
 			return;
 		}
-		props.onAddUser(enteredName, enteredAge);
-		setEnteredUsername("");
-		setEnteredAge("");
+		props.onAddUser(enteredUserName, enteredUserAge);
+		nameInputRef.current.value = "";
+		ageInputRef.current.value = "";
+		// setEnteredUsername("");
+		// setEnteredAge("");
 	};
 
-	const nameChangeHandler = (event) => {
-		setEnteredUsername(event.target.value);
-	};
+	// const nameChangeHandler = (event) => {
+	// 	setEnteredUsername(event.target.value);
+	// };
 
-	const ageChangeHandler = (event) => {
-		setEnteredAge(event.target.value);
-	};
+	// const ageChangeHandler = (event) => {
+	// 	setEnteredAge(event.target.value);
+	// };
 
 	const errorHandler = () => {
 		setFormError(null);
@@ -60,15 +71,17 @@ const AddUser = (props) => {
 					<input
 						id="username"
 						type="text"
-						value={enteredName}
-						onChange={nameChangeHandler}
+						// value={enteredName}
+						// onChange={nameChangeHandler}
+						ref={nameInputRef}
 					/>
 					<label htmlFor="age">Age (Years)</label>
 					<input
 						id="age"
 						type="number"
-						value={enteredAge}
-						onChange={ageChangeHandler}
+						// value={enteredAge}
+						// onChange={ageChangeHandler}
+						ref={ageInputRef}
 					/>
 					<Button type="submit">Add User</Button>
 				</form>
